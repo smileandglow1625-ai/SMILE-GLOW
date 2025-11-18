92f8e01195ffebe07c503918e94dc0d79e0af521
smileandglow1625-ai <smileandglow1625@gmail.com>
2025-11-18 15:59:09 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -23,12 +23,27 @@ app.use(
   cors({
     origin: [
       "https://smileandglowaesthetic.com",
-      "http://localhost:5500"
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://smileandglowaesthetic.com"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 
 
 
